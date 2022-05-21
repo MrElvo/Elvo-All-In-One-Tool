e1e28395bf5135b546b88baa7305b68ebb7ded52
MrElvo <yuval9512@gmail.com>
2022-05-21 17:27:04 +0300

    Update Test Evn.py

--- Test Evn.py
+++ Test Evn.py
@@ -1,48 +1,25 @@
 # # Test Evn
-import hashlib
-import base64
-import AES
-import os
+class Point:
+    def __init__(self, argX, argY):
+        self.x = argX 
+        self.y = argY
+        return
 
 
-
-key = b'1234567890123456'
-# key = os.urandom(16) 
-# iv = os.urandom(16) 
-iv = b'1234567890123456'
-# encrypted = AES.AES(key).encrypt_ctr(b'Thehash', iv)
-
-encrypted = AES.AES(key).encrypt_ctr(input('your hash: '), iv)
-TheChiper = encrypted
-decrypt = AES.AES(key).decrypt_ctr(encrypted, iv)
-
-encoded = base64.b64encode(TheChiper.encode('ascii'))
-Hashstring = hashlib.sha256(TheChiper.encode('UTF-8')).hexdigest()
+    def dst(other_point,arg2X,arg2Y):
+        other_point.x = arg2X
+        other_point.y = arg2Y
+        return 
 
 
+Themath = Point(3,0)
+print(Themath.x)
+print(Themath.y)
+other_point = Point(4,5)
+print(other_point.x)
+print(other_point.y)
 
-print('base64:' , encoded,"\n" + 'sha256: ' ,Hashstring,'\n','AES: ',encrypted)
-print('Decoded Hash')
-print(encoded.decode('ascii'),"\n" ,decrypt)
-
-
-
-# aes = AES.new(key, AES.MODE_CBC, iv)
-# def encrypt_payload(cipher,data):
-#     return cipher.encrypt(data)
-
-# def decrypt_payload(cipher, payload):
-#     return cipher.decrypt(payload)
- 
-# AESdecode = AES.new(key, AES.MODE_GCM, iv)
-# decd = AESChiper.decrypt(bytes(encd))
-
-# print(encrypt_payload(AESChiper,data))
-# print(decrypt_payload(AESChiper,encrypt_payload(AESChiper,data)))
-
-#print("Encrypted data:" + encrypted)
-# print(AESChiper.encrypt(data))
-
+print(((other_point.x- Themath.x)**2) + ((other_point.y - Themath.y)**2)**0.5)
 
 
 
