2c45847382a35956c215ecaf66ebcfcc137f95f6
MrElvo <yuval9512@gmail.com>
2022-05-21 18:28:07 +0300

    Update Test Evn.py

--- Test Evn.py
+++ Test Evn.py
@@ -1,42 +1,6 @@
 # # Test Evn
 
-class Point:
-    def __init__(self, argX, argY):
-        self.x = argX 
-        self.y = argY
-        return
 
-
-    def dst(other_point,arg2X,arg2Y):
-        other_point.x = arg2X
-        other_point.y = arg2Y
-        return 
-
-
-<<<<<<< Updated upstream
-PointOne = Point(10,15)
-print(PointOne.x)
-print(PointOne.y)
-PointTwo = Point(20,30)
-print(PointTwo.x)
-print(PointTwo.y)
-=======
-Themath = Point(3,0)
-print(Themath.x)
-print(Themath.y)
-other_point = Point(4,0)
-print(other_point.x)
-print(other_point.y)
->>>>>>> Stashed changes
-
-print((((PointTwo.x - PointOne.x)**2) + ((PointTwo.y - PointOne.y)**2))**0.5)
-
-
-<<<<<<< Updated upstream
-# √((x_2-x_1)²+(y_2-y_1)²)
-
-=======
->>>>>>> Stashed changes
 
 
 
