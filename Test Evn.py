4079b29149afb66c5c65cf0b3cac5be3c971f29f
MrElvo <yuval9512@gmail.com>
2022-07-24 19:36:11 +0300

    new
    new

--- Test Evn.py
+++ Test Evn.py
@@ -1,26 +1,74 @@
-from difflib import Differ
+# from difflib import Differ
+#
+# print('''
+# Welcome to the File Compartor Section.
+# ---Code Meaning---
+# ‘-‘ line unique to sequence 1
+# ‘+’ line unique to sequence 2
+# ‘ 'line common to both sequences
+# ‘?’ line not present in either input sequence
+# ---Code Meaning---
+#
+# DON'T FORGET TO ADD .txt TO THE FILE IN ORDER FOR IT TO WORK
+# ''')
+#
+# FILEONE = input('File 1 ...: ')
+# FILETWO = input('File 2 ...: ')
+#
+# with open(FILEONE) as file_1, open(FILETWO) as file_2:
+# 	differ = Differ()
+#
+#
+# 	for line in differ.compare(file_1.readlines(), file_2.readlines()):
+# 		print(line)
+#
+#
+#
+# Python program to test
+# internet speed
 
-print('''
-Welcome to the File Compartor Section.
----Code Meaning---
-‘-‘ line unique to sequence 1
-‘+’ line unique to sequence 2
-‘ 'line common to both sequences
-‘?’ line not present in either input sequence
----Code Meaning---
+# import speedtest
 
-DON'T FORGET TO ADD .txt TO THE FILE IN ORDER FOR IT TO WORK
-''')
+# st = speedtest.Speedtest()
+# ds = st.download()
+# st = speedtest.Speedtest()
+# us = st.upload()
+# pn = st.results.ping
+#
+# def humansize(nbytes):
+#     suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+#     i = 0
+#     while nbytes >= 1024 and i < len(suffixes)-1:
+#         nbytes /= 1024.
+#         i += 1
+#     f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+#     return '%s %s' % (f, suffixes[i])
+#
+# #Readable
+# print('Download Speed:',humansize(ds))
+# print('Upload Speed:',humansize(us))
+# print('Ping:',pn)
 
-FILEONE = input('File 1 ...: ')
-FILETWO = input('File 2 ...: ')
+# import urllib.request
+# httpsweb = input('For HTTPS...:')
+# httpweb = input('For HTTP...:')
+# print(urllib.request.urlopen(f"https://www.{httpsweb}.com").getcode())
+# print(urllib.request.urlopen(f"http://www.{httpweb}.com").getcode())
 
-with open(FILEONE) as file_1, open(FILETWO) as file_2:
-	differ = Differ()
 
+import requests
 
-	for line in differ.compare(file_1.readlines(), file_2.readlines()):
-		print(line)
+URL = "www.google.com"
 
+try:
+    response = requests.head(URL)
+except Exception as e:
+    print(f"NOT OK: {str(e)}")
+else:
+    if response.status_code == 200:
+        print("OK")
+    else:
+        print(f"NOT OK: HTTP response code "
+              f"{response.status_code}")
 
 
--- TestMain.py
+++ TestMain.py
@@ -1,5 +1,6 @@
 # ----------Banner--------
 import pathlib
+from time import sleep
 
 from pyfiglet import Figlet
 # from termcolor import colored, cprint
@@ -55,15 +56,15 @@ from bcrypt import hashpw, gensalt, checkpw
 import subprocess
 # -----------Compartor-------
 from difflib import Differ
-
+# -----------Speed Test-------
+import speedtest
 # -----------IMPORTS-------
 
 # TODO: -----------TODO-------
 '''
 - Add English only to the Password Checker Hasher
-- Add File with Passwords to hash
 -Error Check
-- Internet speed teseter + info about your network system
+
 
 -sniffing dog art
 
@@ -94,6 +95,7 @@ Welcome to the Main Menu!
  | For | Password Checker+Hasher > [3]
  | For | Wifi Intelligence >       [4]
  | For | File Compartor >          [5]
+ | For | Speed Test >              [6]
 --------------------------------------
 
  '''))
@@ -247,9 +249,11 @@ Something Isn't right
 
                 else:
                     print('''
-                    \n-----ERROR-----
-                    Something Isn't right
-                    -----ERROR-----\n''')
+                    \n
+-----ERROR-----
+Something Isn't right
+-----ERROR-----
+                    \n''')
                     return QUESTIONBRUTEFORCE()
 
             QUESTIONBRUTEFORCE()
@@ -953,6 +957,78 @@ Something Isn't right, Path does not exists.
 
 
             # -----------------------File Compartor-----------------------
+
+            # -----------------------Speed Test-----------------------
+        elif User_Choice == '6':
+            print('''
+Welcome To The Speed Test Section
+Your Speed Test has been started''')
+            def SPEEDTEST():
+                st = speedtest.Speedtest()
+                ds = st.download()
+                st = speedtest.Speedtest()
+                us = st.upload()
+                pn = st.results.ping
+
+                def humansize(nbytes):
+                    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+                    i = 0
+                    while nbytes >= 1024 and i < len(suffixes) - 1:
+                        nbytes /= 1024.
+                        i += 1
+                    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+                    return '%s %s' % (f, suffixes[i])
+
+                # Readable
+                print(wipe)
+                print('''  
+              ;~~,__
+:-....,-------'`-'._.'
+`-,,,  ,       ;'~~'
+   ,'_,'~.__; '--.
+  //'       ````(;
+                      ''')
+                print('''All Done
+Scan Details ''')
+                print('Download Speed:', humansize(ds))
+                print('Upload Speed:', humansize(us))
+                print('Ping:', pn)
+                def SPEEDQ():
+                    sleep(8)
+                    print(wipe)
+                    print('''
+ /^ ^\/
+/ 0 0 \/
+V\ Y /V
+ / - \/
+ |    \/
+ || (__V   
+ ''')
+                    print('''
+Back to Main Menu OR another Speed Test
+Speed Test >      [1]
+Main Menu >       [2]''')
+                    SPEEDQINP = input(
+'...: ')
+                    if SPEEDQINP =='1':
+                        print(wipe)
+                        print('Another scan has been started ')
+                        return SPEEDTEST()
+                    elif SPEEDQINP =='2':
+                        print(wipe)
+                        print('\n>>>Back To the Main Menu>>>\n')
+                        object.HackTools('Elvo.a')
+                    else:
+                        print(wipe)
+                        print('''
+\n-----ERROR-----
+Something Isn't right.
+-----ERROR-----\n''')
+                        return SPEEDQ()
+                SPEEDQ()
+            SPEEDTEST()
+            # -----------------------Speed Test-----------------------
+
 
 
         else:
